6f02277e3643c3b7b34a6236a59f87586625050b
vector-of-bool <vectorofbool@gmail.com>
2024-08-14 17:52:24 -0600

    everything-header

---
+++ include/amongoc/amongoc.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "./async.h"
+#include "./box.h"
+#include "./client.h"
+#include "./default_loop.h"
+#include "./emitter.h"
+#include "./handler.h"
+#include "./operation.h"
+
+#include "./bson/build.h"
