edea309377fb5223f3b3ece5f9f7f2603f375270
vector-of-bool <vectorofbool@gmail.com>
2024-08-14 13:29:37 -0600

    just<T>

---
+++ src/amongoc/nano/just.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "./concepts.hpp"
+
+#include <neo/attrib.hpp>
+#include <neo/invoke.hpp>
+#include <neo/object_box.hpp>
+#include <neo/object_t.hpp>
+
+#include <utility>
+
+namespace amongoc {
+
+/**
+ * @brief A sender that yields a value immediately.
+ *
+ * The receiver for the sender is invoked inline within the start() for the
+ * generated operation state
+ *
+ * The value is perfect-forwarded along.
+ */
+template <typename T>
+class just {
+public:
+    // Default-construct is only available if the underlying value is defautl-constructbile
+    just() = default;
+
+    // Forward-construct the value in-place
+    constexpr explicit just(T&& arg)
+        : _value(NEO_FWD(arg)) {}
+
+    // We send the exact type that we are given (including references)
+    using sends_type = T;
+
+    /**
+     * @brief Move-connect to a new receiver and return the bound operation
+     */
+    template <nanoreceiver_of<sends_type> R>
+    constexpr nanooperation auto connect(R&& recv) && noexcept {
+        return op<R>{static_cast<T&&>(_value), NEO_FWD(recv)};
+    }
+
+    /**
+     * @brief Copy-connect to a new receiver and return the bound operation
+     *
+     * This overload will copy-construct the bound value into a new operation,
+     * allowing additional connect() to be invoked later on for the same value.
+     */
+    template <nanoreceiver_of<sends_type> R>
+    constexpr nanooperation auto connect(R&& recv) const& noexcept
+        requires std::copy_constructible<sends_type>
+    {
+        return op<R>{static_cast<const T&>(_value), NEO_FWD(recv)};
+    }
+
+private:
+    template <typename R>
+    struct [[nodiscard]] op {
+        NEO_NO_UNIQUE_ADDRESS neo::object_t<T> _value;
+        NEO_NO_UNIQUE_ADDRESS neo::object_t<R> _receiver;
+
+        constexpr void start() noexcept {
+            NEO_INVOKE(static_cast<R&&>(_receiver), static_cast<T&&>(_value));
+        }
+    };
+
+    NEO_NO_UNIQUE_ADDRESS neo::object_t<T> _value;
+};
+
+template <typename T>
+explicit just(T&&) -> just<T>;
+
+}  // namespace amongoc
---
+++ src/amongoc/nano/just.test.cpp
@@ -0,0 +1,39 @@
+#include "./just.hpp"
+#include "./concepts.hpp"
+
+#include <catch2/catch.hpp>
+
+using namespace amongoc;
+
+static_assert(nanosender<just<int>>);
+static_assert(multishot_nanosender<just<int>>);
+static_assert(multishot_nanosender<just<int&>>);
+static_assert(multishot_nanosender<just<const int&>>);
+static_assert(nanosender<just<std::unique_ptr<int>>>);
+// A just() of non-copyable is not multi-shot, since it must move its argument
+static_assert(not multishot_nanosender<just<std::unique_ptr<int>>>);
+// A reference-to-noncopyable is copyable itself, though:
+static_assert(multishot_nanosender<just<std::unique_ptr<int>&>>);
+
+TEST_CASE("Simple just") {
+    auto               x = amongoc::just(42);
+    std::optional<int> oi;
+    auto               op = x.connect([&](auto v) { oi = v; });
+    op.start();
+    CHECK(oi == 42);
+}
+
+TEST_CASE("Copying") {
+    auto                       x = amongoc::just(std::string("hey"));
+    std::optional<std::string> got;
+    {
+        auto op = x.connect([&](auto&& x) { got.emplace(NEO_FWD(x)); });
+        op.start();
+    }
+    CHECK(got == "hey");
+    {
+        auto op = x.connect([&](auto&& x) { got.emplace(NEO_FWD(x)); });
+        op.start();
+    }
+    CHECK(got == "hey");
+}
