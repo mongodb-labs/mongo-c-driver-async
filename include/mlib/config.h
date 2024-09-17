81a552eb217636ebc40524581054f9bff9170cf9
vector-of-bool <vectorofbool@gmail.com>
2024-09-17 13:36:14 -0600

    Remove a C23-ism

--- include/mlib/config.h
+++ include/mlib/config.h
@@ -52,7 +52,7 @@
 
 #define mlib_extern_c_begin() MLIB_IF_CXX(extern "C" {) mlib_static_assert(1, "")
 #define mlib_extern_c_end() MLIB_IF_CXX(                                       \
-  }) mlib_static_assert(1)
+  }) mlib_static_assert(1, "")
 
 /**
  * @brief Use as the prefix of a braced initializer within C headers, allowing
