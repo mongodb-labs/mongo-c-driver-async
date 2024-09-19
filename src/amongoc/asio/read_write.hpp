fbe0f6a758f7aea33a01ee07457131fedd066c4a
vector-of-bool <vectorofbool@gmail.com>
2024-09-19 11:37:37 -0600

    Asio buffer sequence concepts

--- src/amongoc/asio/read_write.hpp
+++ src/amongoc/asio/read_write.hpp
@@ -6,7 +6,28 @@
 #include <asio/read.hpp>
 #include <asio/write.hpp>
 
+#include <concepts>
+#include <iterator>
+
 namespace amongoc {
+
+template <typename T>
+concept const_buffer_iterator = std::forward_iterator<T>
+    and std::convertible_to<std::iter_reference_t<T>, asio::const_buffer>;
+
+template <typename T>
+concept mutable_buffer_iterator = const_buffer_iterator<T>
+    and std::convertible_to<std::iter_reference_t<T>, asio::mutable_buffer>;
+
+template <typename T>
+concept const_buffer_sequence = std::copy_constructible<T> and requires(T bufs) {
+    { asio::buffer_sequence_begin(bufs) } -> const_buffer_iterator;
+};
+
+template <typename T>
+concept mutable_buffer_sequence = const_buffer_sequence<T> and requires(T bufs) {
+    { asio::buffer_sequence_begin(bufs) } -> mutable_buffer_iterator;
+};
 
 template <typename T, typename Bufs = asio::mutable_buffer>
 concept readable_stream = requires(T& strm, Bufs bufs) {
