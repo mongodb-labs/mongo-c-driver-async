bf38ed9a5f2971067b1ea811cec541877469ad5b
vector-of-bool <vectorofbool@gmail.com>
2024-09-23 12:37:45 -0600

    Owning `amongoc::default_loop` type

--- include/amongoc/default_loop.h
+++ include/amongoc/default_loop.h
@@ -18,3 +18,24 @@ extern void amongoc_default_loop_run(amongoc_loop* loop) mlib_noexcept;
 extern void amongoc_default_loop_destroy(amongoc_loop* loop) mlib_noexcept;
 
 mlib_extern_c_end();
+
+#if mlib_is_cxx()
+namespace amongoc {
+
+struct default_event_loop {
+public:
+    default_event_loop() { ::amongoc_default_loop_init(&_loop); }
+    ~default_event_loop() { ::amongoc_default_loop_destroy(&_loop); }
+
+    default_event_loop(default_event_loop&&) = delete;
+
+    void run() noexcept { ::amongoc_default_loop_run(&_loop); }
+
+    amongoc_loop& get() noexcept { return _loop; }
+
+private:
+    amongoc_loop _loop;
+};
+
+}  // namespace amongoc
+#endif
