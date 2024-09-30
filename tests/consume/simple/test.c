1ebb01a3b81d8c7b86b6571393c68264f184acdd
vector-of-bool <vectorofbool@gmail.com>
2024-09-29 21:21:27 -0600

    Add tests for importing the project:
    - Fix up the project for `add_subdirectory()`
    - Fix the installation path for the CMake package files

---
+++ tests/consume/simple/test.c
@@ -0,0 +1,19 @@
+#include <amongoc/amongoc.h>
+
+#undef NDEBUG
+#include <assert.h>
+
+int main() {
+    int             val = 42;
+    amongoc_emitter em
+        = amongoc_just(amongoc_okay, amongoc_box_pointer(&val), mlib_default_allocator);
+    amongoc_status    status;
+    amongoc_box       box;
+    amongoc_operation op = amongoc_tie(em, &status, &box, mlib_default_allocator);
+    amongoc_start(&op);
+    amongoc_operation_destroy(op);
+    assert(status.code == 0);
+    assert(status.category == &amongoc_generic_category);
+    assert(*amongoc_box_cast(int*)(box) == 42);
+    return 0;
+}
