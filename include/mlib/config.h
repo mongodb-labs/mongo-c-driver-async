2297db7903d801380ec22d206cc46ca8776a33b9
vector-of-bool <vectorofbool@gmail.com>
2024-10-21 14:24:15 -0600

    Config header simplification

--- include/mlib/config.h
+++ include/mlib/config.h
@@ -10,71 +10,11 @@
 
 #include <sys/param.h>
 
-#ifdef __cplusplus
-#define MLIB_IF_CXX(...) __VA_ARGS__
-#define MLIB_IF_NOT_CXX(...)
-#else
-#define MLIB_IF_CXX(...)
-#define MLIB_IF_NOT_CXX(...) __VA_ARGS__
-#endif
-
-// Expands to `1` when compiled as C++, otherwise `0`
-#define mlib_is_cxx() MLIB_IF_CXX(1) MLIB_IF_NOT_CXX(0)
-// Expands to `0` when compiled as C++, otherwise `1`
-#define mlib_is_not_cxx() MLIB_IF_CXX(0) MLIB_IF_NOT_CXX(1)
-
 /**
- * @brief Expands to `noexcept` when compiled as C++, otherwise expands to
- * an empty attribute
+ * @brief A function-like macro that always expands to nothing
  */
-#define mlib_noexcept MLIB_IF_CXX(noexcept) MLIB_IF_NOT_CXX([[]])
-
-/**
- * @brief Expands to `constexpr` when compiled as C++, otherwise `inline`
- */
-#define mlib_constexpr MLIB_IF_CXX(constexpr) MLIB_IF_NOT_CXX(inline)
-
-/**
- * @brief Expands to an `alignas()` attribute for the current language
- */
-#define mlib_alignas(T) MLIB_IF_CXX(alignas(T)) MLIB_IF_NOT_CXX(_Alignas(T))
-#define mlib_alignof(T) MLIB_IF_CXX(alignof(T)) MLIB_IF_NOT_CXX(_Alignof(T))
-
-#define mlib_is_little_endian() (__BYTE_ORDER == __LITTLE_ENDIAN)
-
-/**
- * @brief Expands to a `thread_local` specifier for the current language
- */
-#define mlib_thread_local                                                      \
-  MLIB_IF_CXX(thread_local) MLIB_IF_NOT_CXX(_Thread_local)
-
-/**
- * @brief Expands to a `static_assert` for the current language
- */
-#define mlib_static_assert                                                     \
-  MLIB_IF_CXX(static_assert) MLIB_IF_NOT_CXX(_Static_assert)
-
-#define mlib_extern_c_begin() MLIB_IF_CXX(extern "C" {) mlib_static_assert(1, "")
-#define mlib_extern_c_end() MLIB_IF_CXX(                                       \
-  }) mlib_static_assert(1, "")
-
-/**
- * @brief Use as the prefix of a braced initializer within C headers, allowing
- * the initializer to appear as a compound-init in C and an equivalent braced
- * aggregate-init in C++
- */
-#define mlib_init(T) MLIB_IF_CXX(T) MLIB_IF_NOT_CXX((T))
-
-#define mlib_is_consteval()                                                    \
-  MLIB_IF_CXX(::mlib::is_constant_evaluated()) MLIB_IF_NOT_CXX(0)
-
-/**
- * @brief (C++ only) Expands to a `static_cast` expression that perfect-forwards
- * the argument.
- */
-#define mlib_fwd(...) (static_cast<decltype(__VA_ARGS__) &&>(__VA_ARGS__))
-
 #define MLIB_NOTHING(...)
+
 #define MLIB_PASTE(A, ...) _mlibPaste1(A, __VA_ARGS__)
 #define MLIB_PASTE_3(A, B, ...) MLIB_PASTE(A, MLIB_PASTE(B, __VA_ARGS__))
 #define MLIB_PASTE_4(A, B, C, ...)                                             \
@@ -86,7 +26,13 @@
 #define MLIB_STR(...) _mlibStr(__VA_ARGS__)
 #define _mlibStr(...) #__VA_ARGS__
 
+/**
+ * @brief Expands to the first macro argument unconditionally
+ */
 #define MLIB_FIRST_ARG(A, ...) A
+/**
+ * @brief If the argument list if empty, expands to 1. Otherwise, expands to 0
+ */
 #define MLIB_IS_EMPTY(...) MLIB_FIRST_ARG(__VA_OPT__(0, ) 1, ~)
 #define MLIB_IS_NOT_EMPTY(...) MLIB_FIRST_ARG(__VA_OPT__(1, ) 0, ~)
 
@@ -96,6 +42,12 @@
 #define MLIB_EVAL_4(...) MLIB_EVAL_2(MLIB_EVAL_2(__VA_ARGS__))
 #define MLIB_EVAL_2(...) MLIB_EVAL_1(MLIB_EVAL_1(__VA_ARGS__))
 #define MLIB_EVAL_1(...) __VA_ARGS__
+
+/**
+ * @brief Expands to the given argument list, suppressing macro argument
+ * expansion during expansion.
+ */
+#define MLIB_JUST(...) __VA_ARGS__ MLIB_NOTHING(#__VA_ARGS__)
 
 /**
  * @brief Expands to an integer literal corresponding to the number of macro
@@ -109,21 +61,91 @@
   _16
 
 /**
- * @brief Expand to the first argument if `Cond` is 1, the second argument if
- * `Cond` is 0
+ * @brief If the argument expands to `0`, `false`, or nothing, expands to `0`.
+ * Otherwise expands to `1`.
  */
-#define MLIB_IF_ELSE(Cond, IfTrue, IfFalse)                                    \
-  /* Suppress expansion of the two branches by using the '#' operator */       \
-  MLIB_NOTHING(#IfTrue, #IfFalse)                                              \
-  /* Concat the cond 1/0 with a prefix macro: */                               \
-  MLIB_PASTE(_mlibIfElsePick_, Cond)(IfTrue, IfFalse)
+#define MLIB_BOOLEAN(...)                                                      \
+  MLIB_IS_NOT_EMPTY(_mlibPaste1(_mlibBool_, __VA_ARGS__))
+#define _mlibBool_0
+#define _mlibBool_false
+#define _mlibBool_
 
-#define _mlibIfElsePick_1(IfTrue, IfFalse)                                     \
-  /* Expand the first operand, throw away the second */                        \
-  IfTrue MLIB_NOTHING(#IfFalse)
-#define _mlibIfElsePick_0(IfTrue, IfFalse)                                     \
-  /* Expand to the second operand, throw away the first */                     \
-  IfFalse MLIB_NOTHING(#IfTrue)
+/**
+ * @brief A ternary macro. Expects three parenthesized argument lists in
+ * sequence.
+ *
+ * If the first argument list is a truthy value, expands to the second argument
+ * list. Otherwise, expands to the third argument list. The unused argument list
+ * is not expanded and is discarded.
+ */
+#define MLIB_IF_ELSE(...)                                                      \
+  MLIB_PASTE(_mlibIfElseBranch_, MLIB_BOOLEAN(__VA_ARGS__))
+#define _mlibIfElseBranch_1(...) __VA_ARGS__ _mlibNoExpandNothing
+#define _mlibIfElseBranch_0(...) MLIB_NOTHING(#__VA_ARGS__) MLIB_JUST
+#define _mlibNoExpandNothing(...) MLIB_NOTHING(#__VA_ARGS__)
+
+#ifdef __cplusplus
+#define mlib_is_cxx() 1
+#define mlib_is_not_cxx() 0
+#define MLIB_IF_CXX(...) __VA_ARGS__
+#define MLIB_IF_NOT_CXX(...)
+#else
+#define mlib_is_cxx() 0
+#define mlib_is_not_cxx() 1
+#define MLIB_IF_CXX(...)
+#define MLIB_IF_NOT_CXX(...) __VA_ARGS__
+#endif
+
+#define MLIB_LANG_PICK MLIB_IF_ELSE(mlib_is_not_cxx())
+
+/**
+ * @brief Expands to `noexcept` when compiled as C++, otherwise expands to
+ * an empty attribute
+ */
+#define mlib_noexcept MLIB_LANG_PICK([[]])(noexcept)
+
+/**
+ * @brief Expands to `constexpr` when compiled as C++, otherwise `inline`
+ */
+#define mlib_constexpr MLIB_LANG_PICK(inline)(constexpr)
+
+/**
+ * @brief Expands to an `alignas()` attribute for the current language
+ */
+#define mlib_alignas(T) MLIB_LANG_PICK(_Alignas)(alignas)(T)
+#define mlib_alignof(T) MLIB_LANG_PICK(_Alignof)(alignof)(T)
+
+#define mlib_is_little_endian()                                                \
+  mlib_parenthesized_expression(__BYTE_ORDER == __LITTLE_ENDIAN)
+
+/**
+ * @brief Expands to a `thread_local` specifier for the current language
+ */
+#define mlib_thread_local MLIB_LANG_PICK(_Thread_local)(thread_local)
+
+/**
+ * @brief Expands to a `static_assert` for the current language
+ */
+#define mlib_static_assert MLIB_LANG_PICK(_Static_assert)(static_assert)
+
+#define mlib_extern_c_begin() MLIB_IF_CXX(extern "C" {) mlib_static_assert(1, "")
+#define mlib_extern_c_end() MLIB_IF_CXX(                                       \
+  }) mlib_static_assert(1, "")
+
+/**
+ * @brief Use as the prefix of a braced initializer within C headers, allowing
+ * the initializer to appear as a compound-init in C and an equivalent braced
+ * aggregate-init in C++
+ */
+#define mlib_init(T) MLIB_LANG_PICK((T))(T)
+
+#define mlib_is_consteval() MLIB_LANG_PICK(0)(mlib::is_constant_evaluated())
+
+/**
+ * @brief (C++ only) Expands to a `static_cast` expression that perfect-forwards
+ * the argument.
+ */
+#define mlib_fwd(...) (static_cast<decltype(__VA_ARGS__) &&>(__VA_ARGS__))
 
 #ifdef __has_include
 #if __has_include(<mlib.tweaks.h>)
@@ -140,12 +162,29 @@
 #endif // mlib_audit_allocator_passing
 
 #ifdef __GNUC__
-#define mlib_is_gnu_like() true
-#define mlib_is_msvc() false
-#elif _MSC_VER
-#define mlib_is_gnu_like() false
-#define mlib_is_msvc() true
+#define mlib_is_gnu_like() 1
+#ifdef __clang__
+#define mlib_is_gcc() 0
+#define mlib_is_clang() 1
+#else
+#define mlib_is_gcc() 1
+#define mlib_is_clang() 0
 #endif
+#define mlib_is_msvc() 0
+#elif _MSC_VER
+#define mlib_is_gnu_like() 0
+#define mlib_is_msvc() 1
+#endif
+
+#define MLIB_IF_CLANG(...)                                                     \
+  MLIB_IF_ELSE(mlib_is_clang())(__VA_ARGS__)(MLIB_NOTHING(#__VA_ARGS__))
+#define MLIB_IF_GCC(...)                                                       \
+  MLIB_IF_ELSE(mlib_is_gcc())(__VA_ARGS__)(MLIB_NOTHING(#__VA_ARGS__))
+#define MLIB_IF_MSVC(...)                                                      \
+  MLIB_IF_ELSE(mlib_is_msvc())(__VA_ARGS__)(MLIB_NOTHING(#__VA_ARGS__))
+
+#define MLIB_IF_GNU_LIKE(...)                                                  \
+  MLIB_IF_GCC(__VA_ARGS__) MLIB_IF_CLANG(__VA_ARGS__)
 
 #if mlib_is_msvc()
 #define mlib_no_unique_address [[msvc::no_unique_address]]
@@ -153,24 +192,7 @@
 #define mlib_no_unique_address [[no_unique_address]]
 #endif
 
-#if mlib_is_cxx()
-namespace mlib {
-
-static mlib_constexpr bool is_constant_evaluated() noexcept {
-#ifdef __cpp_if_consteval
-  if consteval {
-    return true;
-  } else {
-    return false;
-  }
-#endif
-  return false;
-}
-
-} // namespace mlib
-#endif
-
-#define mlib_nodiscard(Msg) MLIB_IF_CXX([[nodiscard(Msg)]])
+#define mlib_nodiscard(Msg) MLIB_LANG_PICK([[]])([[nodiscard(Msg)]])
 
 #define MLIB_RETURNS(...)                                                      \
   noexcept(noexcept(__VA_ARGS__))                                              \
@@ -180,23 +202,6 @@ static mlib_constexpr bool is_constant_evaluated() noexcept {
     return __VA_ARGS__;                                                        \
   }                                                                            \
   static_assert(true)
-
-#define MLIB_IF_CLANG(...)
-#define MLIB_IF_GCC(...)
-#define MLIB_IF_MSVC(...)
-#if __clang__
-#undef MLIB_IF_CLANG
-#define MLIB_IF_CLANG(...) __VA_ARGS__
-#elif __GNUC__
-#undef MLIB_IF_GCC
-#define MLIB_IF_GCC(...) __VA_ARGS__
-#elif _MSC_VER
-#undef MLIB_IF_MSVC
-#define MLIB_IF_MSVC(...) __VA_ARGS__
-#endif
-
-#define MLIB_IF_GNU_LIKE(...)                                                  \
-  MLIB_IF_GCC(__VA_ARGS__) MLIB_IF_CLANG(__VA_ARGS__)
 
 #define mlib_always_inline                                                     \
   MLIB_IF_GNU_LIKE(__attribute__((always_inline)))                             \
@@ -224,9 +229,23 @@ static mlib_constexpr bool is_constant_evaluated() noexcept {
 
 #define mlib_extern_c MLIB_IF_CXX(extern "C") MLIB_IF_NOT_CXX(extern)
 
+#define mlib_parenthesized_expression(...)                                     \
+  MLIB_IF_CXX(mlib::identity{})(__VA_ARGS__)
+
 #if mlib_is_cxx()
 
 namespace mlib {
+
+mlib_constexpr bool is_constant_evaluated() noexcept {
+#ifdef __cpp_if_consteval
+  if consteval {
+    return true;
+  } else {
+    return false;
+  }
+#endif
+  return false;
+}
 
 /**
  * @brief An invocable object that simply returns its argument unchanged
@@ -241,6 +260,3 @@ struct identity {
 } // namespace mlib
 
 #endif // C++
-
-#define mlib_parenthesized_expression(...)                                     \
-  MLIB_IF_CXX(mlib::identity{})(__VA_ARGS__)
