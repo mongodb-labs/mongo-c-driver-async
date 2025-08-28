50c86185f7ff0316a46128779e4a03b6030a7a89
vector-of-bool <vectorofbool@gmail.com>
2025-08-28 11:01:01 -0600

    URI: Comma-split using subrange(), not string_view

--- src/amongoc/uri.cpp
+++ src/amongoc/uri.cpp
@@ -94,7 +94,7 @@ result<connection_uri> connection_uri::parse(std::string_view                   
     auto comma_split = [](pct_string_view sv) {
         using is_comma = after<std::equal_to<>, ct_constant<','>>;
         using splitter = neo::simple_token_splitter<neo::charclass_splitter<is_comma>>;
-        auto tokens    = neo::tokenizer(std::string_view(sv), splitter{});
+        auto tokens    = neo::tokenizer(std::ranges::subrange(sv), splitter{});
         return std::views::transform(decay_copy(tokens), [](auto sub) {
             return pct_string_view(sub.data(), sub.size());
         });
